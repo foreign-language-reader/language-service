52019adaf220e9a251f2a288105aaab23c6101a9
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-10 13:19:23 -0800

    Don't test removed functionality

--- tests/service/definition/test_definition.py
+++ tests/service/definition/test_definition.py
@@ -68,17 +68,6 @@ def test_get_definitions_for_group_with_errors(mocker):
     )
 
 
-def test_can_pass_through_chinese_definitions(mocker):
-    chinese = mocker.patch(
-        "language_service.service.definition.get_chinese_definitions"
-    )
-    chinese.return_value = "所有的"
-
-    assert get_definitions("CHINESE", "所有的") == "所有的"
-
-    chinese.assert_called_once_with("所有的")
-
-
 def test_can_pass_through_english_definitions(mocker):
     english = mocker.patch(
         "language_service.service.definition.get_english_definitions"
@@ -88,17 +77,6 @@ def test_can_pass_through_english_definitions(mocker):
     assert get_definitions("ENGLISH", "any") == "any"
 
     english.assert_called_once_with("any")
-
-
-def test_can_pass_through_spanish_definitions(mocker):
-    spanish = mocker.patch(
-        "language_service.service.definition.get_spanish_definitions"
-    )
-    spanish.return_value = "cualquier"
-
-    assert get_definitions("SPANISH", "cualquier") == "cualquier"
-
-    spanish.assert_called_once_with("cualquier")
 
 
 def test_raises_exception_on_unknown_language(mocker):
