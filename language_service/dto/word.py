7634771f562c938a9163380db1dbdd6f1addecd8
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-10 23:58:33 -0800

    Match DTO for api

--- language_service/dto/word.py
+++ language_service/dto/word.py
@@ -2,16 +2,20 @@ from marshmallow import Schema, fields
 
 
 class Word:
-    def __init__(self, token="", tag="", lemma=""):  # nosec this is not a password
+    def __init__(self, token="", tag="", lemma="", definitions=None):  # nosec this is not a password
+        if definitions is None:
+            definitions = []
         self.token = token
         self.tag = tag
         self.lemma = lemma
+        self.definitions = definitions
 
     def __repr__(self):
-        return "Word(token: %s, tag: %s, lemma: %s)" % (
+        return "Word(token: %s, tag: %s, lemma: %s, definitions: %s)" % (
             self.token,
             self.tag,
             self.lemma,
+            self.definitions
         )
 
 
@@ -19,3 +23,4 @@ class WordSchema(Schema):
     token = fields.Str()
     tag = fields.Str()
     lemma = fields.Str()
+    definitions = fields.List(fields.Str())
