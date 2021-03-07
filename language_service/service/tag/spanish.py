9e46eedff884924eff8c7b72c8e992a68efe86bc
Lucas Kjaero <lucas@lucaskjaero.com>
2021-03-07 00:45:44 -0800

    Return all the words, not unique ones.

--- language_service/service/tag/spanish.py
+++ language_service/service/tag/spanish.py
@@ -6,9 +6,8 @@ parser = es_core_news_sm.load()
 
 
 def tag_spanish(text):
-    unique_words = {
-        word.text: Word(token=word.text, tag=word.pos_, lemma=word.lemma_)
+    return [
+        Word(token=word.text, tag=word.pos_, lemma=word.lemma_)
         for word in parser(text)
         if is_not_punctuation(word.text)
-    }
-    return list(unique_words.values())
+    ]
