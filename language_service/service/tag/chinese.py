388818f4ccf599ccaa478894686790b2aa10bc4b
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2021-03-07 01:10:32 -0800

    Return all the words, not unique ones. (#13)
    Words should be returned as they occur in the text, not filtered by uniqueness. 
    
    Why? Two reasons:
    - Choosing what to display is a user experience concern, and is best handled there. That means API or the clients. 
    - Users get confused if their words just disappear.
    
    Historically the definitions and tagging were coupled together, so it made sense to reduce data transfer from an engineering perspective. It's the opposite from a user experience perspective.

--- language_service/service/tag/chinese.py
+++ language_service/service/tag/chinese.py
@@ -6,9 +6,8 @@ parser = zh_core_web_sm.load()
 
 
 def tag_chinese(text):
-    unique_words = {
-        word.text: Word(token=word.text, tag=word.pos_, lemma=word.lemma_)
+    return [
+        Word(token=word.text, tag=word.pos_, lemma=word.lemma_)
         for word in parser(text)
         if is_not_punctuation(word.text)
-    }
-    return list(unique_words.values())
+    ]
